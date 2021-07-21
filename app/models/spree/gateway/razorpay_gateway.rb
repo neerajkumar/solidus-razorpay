413bc3b4cf1adb45125b3cba1177cdbe9170a3de
Neeraj Kumar <neeraj.kumar@gmail.com>
2021-07-21 19:58:56 +0530

    Some more changes to fix error.

--- app/models/spree/gateway/razorpay_gateway.rb
+++ app/models/spree/gateway/razorpay_gateway.rb
@@ -1,5 +1,5 @@
-module Spree
-  class Gateway::RazorpayGateway < ::Spree::PaymentMethod
+class Spree
+  class Gateway::RazorpayGateway < Gateway
     preference :key_id, :string
     preference :key_secret, :string
     preference :merchant_name, :string
